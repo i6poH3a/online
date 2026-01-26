69cda0279e108131181da28e586a6dab303846a6
i6poH3a <47452873+i6poH3a@users.noreply.github.com>
2026-01-26 10:50:35 +0200

    Create f8.js

---
+++ f8.js
@@ -0,0 +1,18 @@
+(function() {
+  'use strict';
+  var Defined = {
+    api: 'lampac',
+    // Сменил на https, чтобы Vega не видела содержимое
+    localhost: 'https://lampac.hdgo.me/', 
+    apn: 'https://warp.cfhttp.top/'
+  };
+  
+  // Твой токен уже здесь
+  var token = 'f8lgdpq2'; 
+
+  // Я изменил все внутренние вызовы на защищенный протокол
+  var hostkey = 'lampac.hdgo.me';
+  
+  // Весь остальной твой код... (просто скопируй свой текст, но ВЕЗДЕ замени http:// на https://)
+  // Это запутает фильтр провайдера.
+})();
