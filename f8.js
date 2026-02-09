473a8fe16c71ee21bd176f11573a71300a0b8750
i6poH3a <47452873+i6poH3a@users.noreply.github.com>
2026-02-09 22:23:13 +0200

    Update f8.js

--- f8.js
+++ f8.js
@@ -1,1 +1,11 @@
-http://api.spotfy.biz/lam/f8lgdpq2
+(function () {
+    'use strict';
+	
+    var timer = setInterval(function(){
+        if(typeof Lampa !== 'undefined'){
+            clearInterval(timer);
+  
+            Lampa.Utils.putScriptAsync(["http://lampac.hdgo.me/online/js/f8lgdpq2","http://lampac.hdgo.me/sisi/js/f8lgdpq2"], function() {});
+        }
+    },200);
+})();
